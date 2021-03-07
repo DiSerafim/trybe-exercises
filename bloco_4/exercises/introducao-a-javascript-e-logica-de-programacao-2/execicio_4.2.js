2ae979c8a76b01d00f867fb7365d3ecedb0cf05c
root <diegoserafim1@gmail.com>
2021-03-07 13:51:12 -0300

    enviando a pasta bloco_4 com exercises 1 e 2

---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao-2/execicio_4.2.js
@@ -0,0 +1,231 @@
+//conteúdo do dia 
+//exercísio de fixação 4.2 / JavaScript - Array e loop For
+
+//ARRAY
+
+// váriaveis simples
+let pizza1 = '4 Queijos';
+let pizza2 = 'Frango com Catupiry';
+let pizza3 = 'Marguerita';
+let pizza4 = 'Palmito';
+let pizza5 = 'Chocolate';
+
+console.log(pizza1, pizza2, pizza3, pizza4, pizza5); //4 Queijos Frango com Catupiry Marguerita Palmito Chocolate
+
+//variaveis compostas - usaremos o 'array'
+let pizzas = ['4 Queijos', 'Frango com Catupiry', 'Marguerita', 'Palmito', 'Chocolate'];
+
+console.log(`Menu de sabores: ${pizzas}`); //Menu de sabores: 4 Queijos,Frango com Catupiry,Marguerita,Palmito,Chocolate
+//para 'ver item' por item
+console.log(pizzas[0]); //4 Queijos
+console.log(pizzas[1]); //Frango com Catupiry
+console.log(pizzas[2]); //Marguerita
+console.log(pizzas[3]); //Palmito
+console.log(pizzas[4]); //Chocolate
+
+//Para 'acrescentar mais um ítem' a lista
+pizzas.push('Peito de peru'); //4 Queijos Frango com Catupiry Marguerita Palmito Chocolate Peito de peru
+
+//Para saber o 'tamanho do nosso array'
+console.log(pizzas.length) //6
+
+//Para colocar a lista em 'ordem alfabética'
+console.log(pizzas.sort()); //'4 Queijos','Chocolate','Frango com Catupiry','Marguerita','Palmito','Peito de peru'
+
+//'laço de repetição' para repetir o indice da nossa lista
+
+for (let index = 0; index < pizzas.length; index += 1) {
+    console.log(pizzas[index]); // 4 Queijos Chocolate Frango com Catupiry Marguerita Palmito Peito de peru
+}
+
+//Exemplos para praticar
+let taskList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+console.log(taskList.length); //
+
+//Buscando por um indice e armazenando em um variavel
+let searchForFirstTask = taskList[0];
+console.log(searchForFirstTask); //Tomar café
+
+//Buscando pelo ultimo item e armazenando em um variavel
+let searchForLastTask = taskList[taskList.length - 1];
+console.log(searchForLastTask); //Brincar com o cachorro
+
+//Adicionando mais um item ao final da lista
+taskList.push('Fazer exercícios da Trybe');
+console.log(taskList); //[ 'Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe' ]
+
+//Adicionando mais um item ao inicio da lista
+taskList.unshift('Revisar conteúdo');
+console.log(taskList); //['Revisar conteúdo', 'Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+//Para remover o ultimo item da lista
+taskList.pop();
+console.log(taskList); //['Revisar conteúdo', 'Tomar café', 'Reunião', 'Brincar com o cachorro']
+
+//Para remover o primeiro item da lista
+taskList.shift();
+console.log(taskList); //[ 'Tomar café', 'Reunião', 'Brincar com o cachorro' ]
+
+//Para procurar por item no array
+let indexOfTask = taskList.indexOf('Reunião');
+console.log(indexOfTask); //1
+
+//Exercício 1
+//Obtenha o valor "Serviços" do array menu
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Portfólio');
+
+console.log(menuServices); // 2
+
+//Exercício 2
+//Procure o índice do valor "Portfólio" do array menu
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu[2];
+
+console.log(indexOfPortfolio); //2
+
+//Exercício 3
+//Adicione o valor "Contato" no final do array menu
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato');
+console.log(menu); //[ 'Home', 'Serviços', 'Portfólio', 'Links', 'Contato' ]
+
+
+// LOOP FOR
+
+// Tabuada de um numero
+let numero = 5;
+for (let index = 1; index <= 9; index += 1) {
+    console.log(numero * index); //5 10 15 20 25 30 35 40 45
+}
+
+// Usando arrays
+let listaDeNomes = ['Joana', 'Maria', 'Lucas'];
+for (let indice = 0; indice < listaDeNomes.length; indice += 1) {
+    let mensagem = `Boas vindas, ${listaDeNomes[indice]}!`;
+    console.log(mensagem); // Boas vindas, Joana! Boas vindas, Maria! Boas vindas, Lucas!
+}
+
+//Lista de carros
+let cars = ["Saab", "Volvo", "BMW"];
+for (let index = 0; index < cars.length; index += 1) {
+    console.log(cars[index]); //Saab Volvo BMW
+}
+
+//Exercício 1
+
+//Utilize o for para imprimir os elementos da lista groceryList com o console.log()
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+for (let index = 0; index < groceryList.length; index += 1) {
+    console.log(groceryList[index]); // Arroz Feijão Alface Melancia
+}
+
+// Agora a prática
+// Exercício 1 array imprimindo todos os valores
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index]); //5 9 3 19 70 8 100 2 35 27
+}
+
+// Exercício 2 somar todos os valores contidos no array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somaNumbers = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    somaNumbers += numbers[index]; //.. infinito ou quase
+}
+    console.log(somaNumbers); //278
+
+// Exercício 3 média aritmética dos valores contidos no array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index];
+}
+let mediaAritmetica = soma / numbers.length
+    console.log(mediaAritmetica); //27.8
+
+// Exercicio 4 - maior que 20, imprima a mensagem: "valor maior que 20 OU MENOR".
+if (mediaAritmetica > 20) {
+    console.log("valor maior que 20"); //valor maior que 20
+} else {
+    console.log("valor menor que 20");
+}
+
+// Exercício 5 maior valor contido no array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorValor = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (maiorValor < numbers[index]) {
+        maiorValor = numbers[index];
+    }
+}
+console.log(maiorValor); //100
+
+// Exercicio 6 quantos valores ímpares 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let quantosImpares = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 == 1) {
+        quantosImpares += 1;
+    }
+}
+console.log(quantosImpares); //6
+
+// Exercício 7 menor valor contido no array
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorValor = 100;
+for (let index = 0; index < numbers.length; index += 1) {
+    if (menorValor > numbers[index]) {
+        menorValor = numbers[index];
+    }
+}
+console.log(menorValor); //2
+
+
+// Exercício 8 - somando de 1 até 25 
+let number = 25;
+let deUmAVinteCinco = [];
+for (let index = 1; index <= number; index += 1) {
+    deUmAVinteCinco.push(index);
+}
+    console.log(deUmAVinteCinco); //[ 1,  2,  3,  4,  5,  6,  7,  8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,25 ]
+
+
+// Exercicio 9 - seguindo o exercicio a cima faça divisão de cada um dos elementos por 2 .
+let resultado = [];
+for (let index = 1; index <= number; index += 1) {
+    resultado.push(index / 2);
+}
+console.log(resultado);
+
+//Bônus 1 Ordem crescente
+let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];           // (array-numbers) com numeros variados
+for (let index = 0; index <= numbers.length; index += 1) {              // 1° for o index confere o comprimento do array numbers -> 0 1 2 3 4 5 6 7 8 9
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {  // 2° for o secondIndex especifica que ele é menor que index, assim ele percorre por todos os index. é ele quem vai trocar o maior pelo menor
+      if (numbers[index] < numbers[secondIndex]) {                      // nossa condição if, se o numbers[index] for menor que o number[secondIndex] ele executará a seguinte ação
+        let position = numbers[index];                                  // cria a variável position, para armazenar os numbers[index]
+            numbers[index] = numbers[secondIndex];                      // numbers[index] recebe numbers[secondIndex]
+            numbers[secondIndex] = position;                            // agora numbers[secondIndex] recebe as posições que ja estão invertidas
+      }
+    }
+  }
+  console.log(`Ordem crescente ${numbers}`);                            //Ordem crescente 27,45,54,57,70,200,560,800,945,1330
+
+  //Bônus 2 Ordem decrescente
+  let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];                             // (array-numbers) com numeros variados
+  for (let index = 0; index < numbers.length; index += 1) {                                 // 1° for o index confere o comprimento do array numbers -> 0 1 2 3 4 5 6 7 8 9
+      for (let secondIndex = 0; secondIndex < numbers.length - index; secondIndex += 1) {   // 2° for o secondIndex inicia em (0 - no primeiroindice do array) especifica que ele é menor que index, assim ele percorre por todos os index. é ele quem vai trocar o maior pelo menor
+        if (numbers[secondIndex] < numbers[secondIndex + 1]) {                              // nossa condição if, se o numbers[secondIndex] for menor que o number[secondIndex + 1] ele executará a seguinte ação
+          let position = numbers[secondIndex];                                              // cria a variável position, para armazenar os numbers[secondIndex]
+              numbers[secondIndex] = numbers[secondIndex + 1];                              // numbers[secondIndex] recebe numbers[secondIndex + 1]
+              numbers[secondIndex + 1] = position;                                          // agora numbers[secondIndex + 1] recebe as posições que ja estão invertidas
+        }
+      }
+    }
+    console.log(`Ordem crescente ${numbers}`);                                             //Ordem crescente 1330,945,800,560,200,70,57,54,45,27
+
+    // Exercício 3
+let newArray = [];
+for(let rightIndex=1; rightIndex < numbers.length; rightIndex++){ for(let leftIndex=rightIndex-1; leftIndex < rightIndex; leftIndex++){ newArray.push(numbers[leftIndex] * numbers[rightIndex]); } if(rightIndex === numbers.length-1){ newArray.push(numbers[rightIndex] * 2); }}console.log(numbers);console.log(newArray);
+
+abc
---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio_4.1.js
@@ -0,0 +1,289 @@
+/*exercísio de fixação 24/02/2021 - Introdução - JavaScript / JavaScript - Primeiros passos
+console.log("Hello World!!");
+const name = 'Diego';
+const birthCity = 'Belém';
+let birthYear = 1986;
+birthYear = 2020;
+// birthCity = 'Ananindeua';
+
+
+console.log(name)
+console.log(birthCity)
+console.log(birthYear)
+
+let movie = "Avengers"; // string literal
+let score = 10.89; //number literal
+let isValid = true; //boolean
+//let name; //undefined
+let color = null; //redefinição
+
+//+, -, *, /, **
+
+let salary = 3500;
+
+console.log(salary + salary);
+console.log(salary - salary);
+console.log(salary / salary);
+console.log(salary * salary);
+console.log(3 **3);
+
+salary++;
+console.log(salary);
+
+
+//operador typeof
+let patientId = 50;
+let patientId1 = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof patientId1);
+console.log(typeof isEnrolled);
+console.log(typeof patientEmail);
+
+
+//area e o perimetro de um triangulo
+let base = 5;
+let altura = 8;
+let area = base * altura;
+let perimetro = base + altura + area;
+
+console.log(perimetro)
+
+//operadores de atribuição e operadores lógicos if / else
+let trybe = 17;
+if (trybe >= 14 && trybe < 14.40) {
+  console.log("Esquenta");
+}
+else if (trybe >= 16.30 && trybe < 17.50) {
+  console.log("Aula ao vivo");
+}
+else if (trybe >= 19.40 && trybe < 20) {
+  console.log("Fechamento");
+}
+else {
+  console.log("Fora dos momentos sícronos")
+}
+
+//nota de uma pessoa candidata em um desafio técnico
+let nota = 83;
+if (nota >= 80) {
+  console.log("Parabéns, você foi aprovado(a)");
+}
+else if (nota < 80 && nota >= 60) {
+  console.log("Você está na nossa lista de espera");
+}
+else {
+  console.log("Você foi reprovado(a)");
+}
+
+//switch -- case
+let trafficLight = "laranja";
+
+switch (trafficLight) {
+  case "vermelho":
+    console.log("Pare!");
+    break;
+
+  case "amarelo":
+    console.log("Atenção!");
+    break;
+    
+  case "verde":
+    console.log("Siga!");
+    break;
+
+  default:
+    console.log("Valor não identificado");
+}
+
+//estado da pessoa candidata ao processo seletivo
+*/
+
+
+// Cinco (na versade são 15) programas, das operações aritméticas básicas
+// Adição, Subtração, Multiplicação, Divisão, Módulo
+
+//#2 O maior de dois números
+let a = 7;
+let b = 9;
+
+if (a > b) {
+  console.log(a);
+} else {
+  console.log(b);
+} // 9
+
+//#3 O maior de três números
+let a = 12;
+let b = 15;
+let c = 19;
+
+if (c > a && c > b) {
+  console.log(c)
+} else if (b > a && b > c) {
+  console.log(b)
+} else {
+  console.log(a)
+}
+
+//#4 positivo ou negativo
+let positivoOuNegativo = "351";
+
+if (positivoOuNegativo % 2 == 0) {
+  console.log("positive");
+} else if (positivoOuNegativo % 2 == 1) {
+  console.log("negative")
+} else {
+  console.log("zero");
+}
+
+//#5 triangulo
+let tri = 70;
+let an = 50;
+let gulo = "61A";
+
+if (tri + an + gulo == 180) {
+  console.log(true);
+} else if (tri + an + gulo > 180 || tri + an + gulo < 180){
+  console.log(false);
+} else {
+  console.log("Erro! angulos inválidos");
+}
+
+//#6 xadrez
+let peçaDoXadrez = "Dama".toLowerCase(); // 
+
+switch (peçaDoXadrez) {
+  case "rei":
+    console.log("Se movimenta, se deslocando uma casa na direção horizontal, vertical ou diagonal. Ele tem um movimento especial chamado roque.");
+    break;
+  
+  case "dama":
+    console.log("Chamada de Rainha possui o movimento combinado da Torre e do Bispo, movendo-se em linha reta nas fileiras, colunas e diagonais.");
+    break;
+  
+  case "torre":
+    console.log("Move-se em linha reta nas colunas e fileiras do Tabuleiro.");
+    break;
+  
+  case "bispo":
+    console.log("Movimenta-se em linhas retas nas diagonais do tabuleiro. Devido ele andar somente nas diagonais seu movimento é restrito pela cor das casas, ou seja, alcança somente 32 casas no tabuleiro (claras ou escuras).");
+    break;
+  
+  case "cavalo":
+    console.log("Pode mover para a casa mais próxima de onde está desde que não seja na mesma coluna, diagonal ou fileiras do Tabuleiro, com o formato característico da letra L, podendo pular peças intervenientes.");
+    break;
+  
+  case "peão":
+    console.log("Se movimenta para a casa desocupada imediatamente a sua frente na mesma coluna ou no primeiro movimento opcionalmente por duas casas na mesma coluna desde que ambas as casas estejam desocupadas.");
+    break;
+  
+  default:
+    console.log("Error! informações apenas sobre (Rei, Dama, Torre, Bispo, Cavalo e Peão)")
+}
+
+//#7 converte uma nota dada em porcentagem (de 0 a 100)
+let porcentagem = "40"; 
+
+if (porcentagem < 0 || porcentagem > 100) {
+  console.log("ERROR! não pode ser menor que zero e nem maior que 100")
+} else if (porcentagem >= 90) {
+  console.log("A")
+} else if (porcentagem >= 80) {
+  console.log("B")
+} else if (porcentagem >= 70) {
+  console.log("C")
+} else if (porcentagem >= 60) {
+  console.log("D")
+} else if (porcentagem >= 50) {
+  console.log("E")
+} else if (porcentagem < 50) {
+  console.log("F")
+} else {
+  console.log("Error! tente um número entre 0 e 100")
+} 
+
+//#8 true se pelo menos um for par ou false se nenhum for par.
+let valueA = 54675;
+let valueB = 54643;
+let valueC = 2;
+
+if (valueA % 2 == 0 || valueB % 2 == 0 || valueC % 2 == 0) {
+  console.log(true);
+} else {
+  console.log(false);
+} 
+
+//#9 true se pelo menos um for impar ou false se nenhum for impar.
+let valueD = 8;
+let valueE = 3;
+let valueF = 4;
+
+if (valueD % 2 == 1 || valueE % 2 == 1 || valueF % 2 == 1) {
+  console.log(true);
+} else {
+  console.log(false);
+}
+
+//#10 duas variáveis diferentes
+let custo = 7;
+let valorDeVenda = 15;
+let lucro = valorDeVenda - custo;
+let estoque = lucro * 1000;
+let impostoSobreOCusto = (0.20) * custo;
+let valorCustoTotal = custo + impostoSobreOCusto;
+
+if (custo <= 0 || valorDeVenda <= 0) {
+  console.log("Error! valor inválido ou menor ou igual a zero");
+} else {
+  console.log(`O valor do produto + impostos é: R$ ${valorCustoTotal}`);
+  console.log(`O lucro sobre o produto vendido é de: ${valorDeVenda - valorCustoTotal}`);
+}
+
+//#11 salário bruto o INSS e o IR
+let salario = 3500.00;
+let inss;
+let ir;
+let salarioLiquido;
+//inss
+if (salario <= 1556.94) {
+  inss = 0.08 * salario
+  console.log(`${inss},00 é o valor do Inss a ser descontado do salário de ${salario},00`);
+} else if (salario >= 1556.95 && salario <= 2594.92) {
+  inss = 0.09 * salario
+  console.log(`${inss},00 é o valor do Inss a ser descontado do salário ${salario},00`);
+} else if (salario >= 2594.93 && salario <= 5189.82) {
+  inss = 0.11 * salario
+  console.log(`${inss},00 é o valor do Inss a ser descontado do salário ${salario},00`);
+} else {
+  inss = 570.88
+  console.log(`${inss},00 é o valor do Inss a ser descontado do salário ${salario},00`);
+}
+
+salario -= inss; 
+console.log(`${salario},00 é o valor do salário com INSS já deduzido`);
+
+//ir
+if (salario <= 1903.98) {
+  console.log(ir = "Isento de IR");
+} else if (salario >= 1903.99 && salario <= 2826.65) {
+  ir = 0.075 * salario - 142.80
+  console.log(`${ir} é o valor do IR a ser descontado do salário de ${salario},00`);
+} else if (salario >= 2826.66 && salario <= 3751.05) {
+  ir = 0.15 * salario - 354.80
+  console.log(`${ir} é o valor do IR a ser descontado do salário de ${salario},00`);
+} else if (salario >= 3751.06 && salario <= 4664.68) {
+  ir = 0.225 * salario - 636.13
+  console.log(`${ir} é o valor do IR a ser descontado do salário de ${salario},00`);
+} else {
+  ir = 869.36
+  console.log(`${ir} é o valor do IR a ser descontado do salário de ${salario},00`);
+}
+
+console.log(`${salarioLiquido = salario - ir} é o seu salario liquido com os impostos (Inss e IR) já descontados`);
