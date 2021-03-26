a8c31561558cf282193351046dd2487b97be8a02
root <diegoserafim1@gmail.com>
2021-03-26 14:46:04 -0300

    Iniciando a questão 02 do exercicio

--- bloco_7/introducao-a-javascript-es6-e-testes-unitarios_3/bloco_7.3.js
+++ bloco_7/introducao-a-javascript-es6-e-testes-unitarios_3/bloco_7.3.js
@@ -81,14 +81,13 @@ function division(x, y) {
   if (y === 0) throw new Error('parameter y must not be 0');
   return x / y;
 }
-
+console
 // Ex-11
 const assert = require('assert');
 assert.strictEqual(typeof myFunction, 'function');
 
 
 // AULA AO VIVO ----------------------------------------------------------------------
-
 // TDD (Test Driven Development);
 
 const assert = require('assert');
@@ -123,3 +122,53 @@ assert.strictEqual(summationOF(5), 15);
 assert.throws(() => {
   summation();
 });
+
+
+// CONTEUDO - Exercícios do dia 7.3 --------------------------------------------------
+// Ex-01
+const assert = require('assert');
+const { strictEqual } = require('assert');
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+// implemente seus testes aqui
+// 1. A função sum(a, b) retorna a soma do parâmetro a com o b
+console.log(sum(4, 5)); // 9 
+// 2. Teste se o retorno de sum(4, 5) é 9
+assert.strictEqual(sum(4, 5), 9); // 9 
+console.log(sum(4, 5)); // 9 
+
+// 3. Teste se o retorno de sum(0, 0) é 0
+assert.strictEqual(sum(0, 0), 0); // 0
+console.log(sum(0, 0)); // 0
+
+// 4. Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
+assert.throws(() => {
+  sum(4, "5");
+});
+
+// 5. Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.strictEqual(sum(4, "5")); // parameters must be numbers
+
+// Ex-02
+const assert = require('assert');
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+      console.log(newArr);
+    }
+  }
+  return newArr;
+}
+// implemente seus testes aqui
+// A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+// Verifique se o array passado por parâmetro não sofreu alterações
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
