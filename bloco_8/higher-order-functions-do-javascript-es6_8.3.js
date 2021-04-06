9c767cb16e3d0e66d896b202c43686573c1be25e
root <diegoserafim1@gmail.com>
2021-04-06 13:42:18 -0300

    Resolvido exercicio_1-de-7, final do dia 8.3

--- bloco_8/higher-order-functions-do-javascript-es6_8.3.js
+++ bloco_8/higher-order-functions-do-javascript-es6_8.3.js
@@ -144,7 +144,7 @@ const products = ['Arroz', 'Feijão', 'Alface', 'Tomate'];
 const prices = [2.99, 3.99, 1.5, 2];
 
 // Deseja-se juntá-las em apenas uma lista de objetos que fique dessa forma:
-const listProducts = [{ Arroz: 2.99 },...];
+// const listProducts = [{ Arroz: 2.99 },...];
 
 // Tente criar uma função que resolva esse problema. Lembre-se, também, que as funções passadas para as HOFs podem receber vários parâmetros, não só o elemento sobre o qual ela está sendo iterada! Use isso em seu favor. Caso tenha dúvidas, olhe a solução logo abaixo.
 const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
@@ -358,7 +358,7 @@ const filter = (array, callback) => {
 console.log(filter(numbers, (number) => number <= 2));
 // -------------------------------------------
 
-
+// find, filter, map
 const states = [
   { short: 'AM', name: 'Amazonas' },
   { short: 'PA', name: 'Pará' },
@@ -414,6 +414,7 @@ console.log(citiesAndStatesNames);
 //   city: "Manaus",
 //   region: "Norte"
 // }
+console.log('Formato: { state: "Amazonas", city: "Manaus", region: "Norte" }');
 const arrayCities = cities.map((city) => {
   const findState = states.find((state) => state.short === city.state);
   const findRegion = regions.find((region) => region.short === city.region);
@@ -425,3 +426,200 @@ const arrayCities = cities.map((city) => {
 });
 
 console.log(arrayCities);
+
+// Exercícios para finalizar o dia
+// Hora de por a mão na massa!
+
+// Instruções para realização dos exercícios
+
+// Nos exercícios a seguir, você trabalhará com uma estrutura de dados representando uma lista de livros, contendo informações como nome do livro, gênero, pessoa autora do livro e data de lançamento.
+
+// Em cada exercício, será pedido que você encontre ou produza alguma informação a respeito dessa lista utilizando as funções que você aprendeu hoje. Todos os exercícios contêm um código base. Você deverá copiar esse código e salvá-lo em um arquivo nomeado conforme o número do exercício, completando a função em branco.
+
+// Por exemplo, o exercício 1 deve ser salvo no arquivo exercise1.js, e assim por diante. Em cada exercício existe uma ou mais chamadas de funções do módulo assert. Estas funções checarão automaticamente se seu código retorna o resultado esperado.
+
+// Sua solução só será considerada correta se todos os asserts do arquivo forem executados sem erros. No Visual Studio Code, você pode executar o código do exemplo clicando com o botão direito e escolhando a opção Run Code.
+
+// Quando todos os asserts passam, isto é, nenhum deles encontra um resultado diferente esperado, nada de diferente do normal é reportado:
+
+const assert = require('assert');
+function foo() {
+  return 'bar';
+}
+assert.strictEqual(foo(), 'bar');
+
+// [Running] node "/Users/leandro/example.js"
+// [Done] exited with code=0 in 0.087 seconds
+// ------------------------------
+
+const assert = require('assert');
+function foo() {
+  return 'bar';
+}
+assert.strictEqual(foo(), 'baz');
+
+// [Running] node "/Users/leandro/example.js"
+// assert.js:92
+//   throw new AssertionError(obj);
+//   ^
+
+// AssertionError [ERR_ASSERTION]: 'bar' == 'baz'
+//     at Object.<anonymous> (/Users/leandro/example.js:7:8)
+//     at Module._compile (internal/modules/cjs/loader.js:956:30)
+//     at Object.Module._extensions..js (internal/modules/cjs/loader.js:973:10)
+//     at Module.load (internal/modules/cjs/loader.js:812:32)
+//     at Function.Module._load (internal/modules/cjs/loader.js:724:14)
+//     at Function.Module.runMain (internal/modules/cjs/loader.js:1025:10)
+//     at internal/main/run_main_module.js:17:11 {
+//   generatedMessage: true,
+//   code: 'ERR_ASSERTION',
+//   actual: 'bar',
+//   expected: 'baz',
+//   operator: '=='
+// }
+
+// [Done] exited with code=1 in 0.075 seconds
+
+/* Agora a prática
+ 1. Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+ Dica: Use a função map */
+const assert = require('assert');
+const books = [
+  { id: 1, name: 'As Crônicas de Gelo e Fogo', genre: 'Fantasia',
+    author: { name: 'George R. R. Martin', birthYear: 1948, },
+    releaseYear: 1991,
+  },
+  { id: 2, name: 'O Senhor dos Anéis', genre: 'Fantasia',
+    author: { name: 'J. R. R. Tolkien', birthYear: 1892, },
+    releaseYear: 1954,
+  },
+  { id: 3, name: 'Fundação', genre: 'Ficção Científica',
+    author: { name: 'Isaac Asimov', birthYear: 1920, },
+    releaseYear: 1951,
+  },
+  { id: 4, name: 'Duna', genre: 'Ficção Científica',
+    author: { name: 'Frank Herbert', birthYear: 1920, },
+    releaseYear: 1965,
+  },
+  { id: 5, name: 'A Coisa', genre: 'Terror',
+    author: { name: 'Stephen King', birthYear: 1947, },
+    releaseYear: 1986,
+  },
+  { id: 6, name: 'O Chamado de Cthulhu', genre: 'Terror',
+    author: { name: 'H. P. Lovecraft', birthYear: 1890, },
+    releaseYear: 1928,
+  },
+];
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+function formatedBookNames() {
+  // escreva seu código aqui
+  const nameFormated = books.map((book) => {
+    return `${book.name} - ${book.genre} - ${book.author.name}`;
+  });
+  console.log(nameFormated);
+}
+assert.deepStrictEqual(formatedBookNames(), expectedResult);
+
+/* 2. Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+Dica: use as funções map, sort */
+const assert = require('assert');
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+function nameAndAge() {
+  // escreva seu código aqui
+  
+}
+assert.deepStrictEqual(nameAndAge(), expectedResult);
