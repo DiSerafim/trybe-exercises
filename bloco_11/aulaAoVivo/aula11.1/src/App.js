2b8c16181a5ed1af8bc3caddeea069767700aa62
root <diegoserafim1@gmail.com>
2021-05-02 19:29:36 -0300

    Criado o conteudo da aula ao vivo

---
+++ bloco_11/aulaAoVivo/aula11.1/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloMyFriends from './components/HelloMyFriends';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <HelloMyFriends />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Documentação do React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ bloco_11/aulaAoVivo/aula11.1/src/components/HelloMyFriends.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function HelloMyFriends() {
+  return (
+    <p>Hello My Friends!!!</p>
+  );
+}
+
+export default HelloMyFriends;
