9af4b5ee26aeda936215fa989cc94e8722efc84b
root <diegoserafim1@gmail.com>
2021-07-31 15:42:09 -0300

    conteudo ao vivo

--- back-end/bloco20/bloco20.1/introducao-a-sql.sql
+++ back-end/bloco20/bloco20.1/introducao-a-sql.sql
@@ -48,7 +48,17 @@
 -- > CONTEÚDO do dia --------- <---/ FIM -----------------------------------------//
 
 -- > AULA ao VIVO ------------ <---/ INICIO --------------------------------------//
--- Explicação Técnica
+
+-- seleciona todas as colunas da tabela address
+SELECT * FROM sakila.address;
+
+-- seleciona as 2 colunas da tabela address
+SELECT district, city_id FROM sakila.address;
+
+-- seleciona um linha especifica da tabela
+SELECT * FROM sakila.address WHERE address_id = 603;
+-- 
+-- 
 -- > AULA ao VIVO ------------ <---/ FIM -----------------------------------------//
 
 -- > EXECÍCIO do dia --------- <---/ INICIO --------------------------------------//
