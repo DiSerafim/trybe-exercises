24831f17da69509ebc6e6cb9b73ed21f85236cbc
root <diegoserafim1@gmail.com>
2021-08-15 15:24:29 -0300

    20.3 em 1 único documento

--- back-end/bloco20/bloco20.3/filtrando-dados-de-forma-especifica.sql
+++ back-end/bloco20/bloco20.3/filtrando-dados-de-forma-especifica.sql
@@ -1,11 +1,11 @@
--- > CONTEÚDO do dia --------- <---/ INICIO --------------------------------------//
+############################## Filtrando dados de forma específica
+-- > CONTEÚDO do dia - 20.1 -- <---/ INICIO --------------------------------------//
 
--- 20.3
--- * Filtrar resultados de consultas com o WHERE .
--- * Utilizar operadores booleanos e relacionais em consultas.
--- * Criar consultas mais dinâmicas e maleáveis com LIKE .
--- * Fazer consultas que englobam uma faixa de resultados com IN e BETWEEN .
--- * Encontrar e separar resultados que incluem datas.
+# Filtrar resultados de consultas com o WHERE .
+# Utilizar operadores booleanos e relacionais em consultas.
+# Criar consultas mais dinâmicas e maleáveis com LIKE .
+# Fazer consultas que englobam uma faixa de resultados com IN e BETWEEN .
+# Encontrar e separar resultados que incluem datas.
 
 -- WHERE
 
@@ -179,4 +179,123 @@ FROM sakila.rental WHERE rental_id = 10330;
 -- Monte uma query que retorne todos os dados do pagamento feito no dia 28/07/2005 a partir das 22 horas.
 SELECT * FROM sakila.payment WHERE DATE(payment_date) = '2005-07-28' AND HOUR(payment_date) >= 22; # 47 row(s) returned
 
--- > CONTEÚDO do dia --------- <---/ FIM -----------------------------------------//
+-- > CONTEÚDO do dia - 20.1 -- <---/ FIM -----------------------------------------//
+##############################
+-- > AULA ao VIVO - 20.1 ----- <---/ INICIO --------------------------------------//
+
+-- WHERE
+-- IN
+-- NOT IN
+-- BETWEEN
+-- LIKE
+
+-- WHERE
+SELECT * FROM sakila.actor;
+SELECT * FROM sakila.actor WHERE first_name = 'johnny';
+SELECT * FROM sakila.actor WHERE first_name = 'johnny' AND last_name = 'Cage';
+SELECT * FROM sakila.actor WHERE first_name = 'johnny' OR last_name = 'Cage';
+SELECT title, rental_duration from sakila.film WHERE rental_duration = 6;
+SELECT title, rental_duration from sakila.film WHERE rental_duration <> 6;
+SELECT title, rental_duration, rating from sakila.film WHERE rental_duration = 6 AND rating = 'r';
+SELECT title, rental_duration, rating from sakila.film WHERE rental_duration = 6 AND rating = 'r' OR rating = 'PG-13';
+SELECT title, rental_duration, rating from sakila.film WHERE title = 'AFFAIR PREJUDICE' OR title = 'AFRICAN EGG' OR title = 'AGENT TRUMAN' OR title = 'AIRPLANE SIERRA';
+
+-- IN
+SELECT * FROM sakila.film;
+SELECT * FROM sakila.film WHERE title IN ('AFFAIR PREJUDICE', 'AFRICAN EGG', 'AGENT TRUMAN', 'AIRPLANE SIERRA');
+
+-- NOT IN
+SELECT * FROM sakila.film WHERE title NOT IN ('AFFAIR PREJUDICE', 'AFRICAN EGG', 'AGENT TRUMAN', 'AIRPLANE SIERRA');
+
+-- BETWEEN
+SELECT * FROM sakila.film;
+SELECT * FROM sakila.film WHERE length BETWEEN 50 AND 130;
+SELECT * FROM sakila.film WHERE length BETWEEN 50 AND 130 ORDER BY length;
+SELECT * FROM sakila.actor WHERE first_name BETWEEN 'b' AND 'd' ORDER BY first_name; # somente o 'd' não funciona 
+SELECT * FROM sakila.actor WHERE first_name BETWEEN 'b' AND 'dZ' ORDER BY first_name; # funciona (gambiarra)
+SELECT * FROM sakila.actor WHERE first_name BETWEEN 'bela' AND 'dustin' ORDER BY first_name;
+SELECT * FROM sakila.rental WHERE rental_date BETWEEN '2005-05-27' AND '2005-06-03';
+
+# '2021-08-06 15:02:32'
+SELECT NOW() AS 'Data e hora atual';
+
+# '2021-08-06'
+SELECT DATE(NOW()) AS 'Data atual';
+
+# '2021-08-06'
+SELECT CURDATE() AS 'Data atual'; 
+
+# '15:07:34'
+SELECT CURTIME() AS 'Hora atual';
+
+# '15'
+SELECT HOUR(NOW()) AS 'Hora atual';
+
+# '15:26:34'
+SELECT TIME(NOW()) AS 'Hora atual';
+
+# '2021'
+SELECT YEAR(NOW()) AS 'Ano';
+
+# '8'
+SELECT MONTH(NOW()) AS 'Mês';
+
+# '6'
+SELECT DAY(now()) AS 'Dia';
+
+SELECT * FROM sakila.rental WHERE DATE(rental_date) = '2005-05-26'; # 174 row(s) returned
+SELECT * FROM sakila.rental WHERE rental_date BETWEEN '2005-05-26 00:00:00' AND '2005-05-26 12:00:00'; # 91 row(s) returned
+
+-- LIKE
+
+SELECT * FROM sakila.customer WHERE first_name LIKE '%fer%'; # tem 'fer' no meio da frase
+SELECT * FROM sakila.customer WHERE first_name LIKE '%dra'; # tem 'dra' no fim da frase
+SELECT * FROM sakila.customer WHERE first_name LIKE 'jun%'; # tem 'jun' no início da frase
+SELECT * FROM sakila.customer WHERE first_name LIKE 'J%E'; # começa com 'J' termina com 'E'
+SELECT * FROM sakila.customer WHERE first_name LIKE '__ME%'; # a 3a e 4a letra são 'ME'... 
+SELECT * FROM sakila.customer WHERE first_name LIKE '____'; # possui 4 caracteres
+
+-- > AULA ao VIVO - 20.1 ----- <---/ FIM -----------------------------------------//
+##############################
+-- > EXERCÍCIO do dia - 20.1 -- <---/ INICIO --------------------------------------//
+
+-- 1 Escreva uma query para exibir todas as peças que começam com GR.
+SELECT * FROM PecasFornecedores.Pecas WHERE name LIKE 'gr%'; # 3 row(s) returned
+-- 2 Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2. Organize o resultado por ordem alfabética de fornecedor.
+SELECT * FROM PecasFornecedores.Fornecimentos WHERE peca = 2 ORDER BY Fornecedor; # 3 row(s) returned
+-- 3 Escreva uma query para exibir as peças, preço e fornecedor de todos os fornecimentos em que o código do fornecedor tenha a letra N.
+SELECT peca, Preco, Fornecedor FROM PecasFornecedores.Fornecimentos WHERE Fornecedor LIKE '%N%'; # 4 row(s) returned
+-- 4 E screva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA). Ordene os resultados em ordem alfabética decrescente.
+SELECT * FROM PecasFornecedores.Fornecedores WHERE name LIKE '%LTDA' ORDER BY name DESC; # 2 row(s) returned
+-- 5 Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código.
+SELECT COUNT(*) FROM PecasFornecedores.Fornecedores WHERE code LIKE '%F%'; # 2
+-- 6 Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00. Ordene os resultados por ordem crescente.
+SELECT * FROM PecasFornecedores.Fornecimentos WHERE Preco BETWEEN 15 AND 40 ORDER BY Preco ASC; # 3 row(s) returned
+-- 7 Escreva uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019.
+SELECT COUNT(*) FROM PecasFornecedores.Vendas WHERE DATE(order_date) BETWEEN '2018-04-15' AND '2019-07-30'; # 3
+
+-- Bônus
+-- Exercício 3
+-- Use o banco de dados Scientists restaurado no dia anterior.
+SELECT * FROM Scientists;
+
+-- 1 Escreva uma query para exibir todas as informações de todos os cientistas que possuam a letra 'e' em seu nome.
+SELECT * FROM Scientists.Scientists WHERE Name LIKE '%e%'; # 12 row(s) returned
+-- 2 Escreva uma query para exibir o nome de todos os projetos cujo o código inicie com a letra A. Ordene o resultado em ordem alfabética.
+SELECT Name FROM Scientists.Projects WHERE Code LIKE 'A%' ORDER BY Name; # 9 row(s) returned
+-- 3 Escreva uma query para exibir o código e nome de todos os projetos que possuam em seu código o número 3. Ordene o resultado em ordem alfabética.
+SELECT Code, Name FROM Scientists.Projects WHERE Code LIKE '%3%' ORDER BY Name; # 3 row(s) returned
+-- 4 Escreva uma query para exibir todos os cientistas (valores numéricos) cujos projetos sejam AeH3, Ast3 ou Che1.
+SELECT Scientist FROM Scientists.AssignedTo WHERE Project IN('AeH3', 'Ast3', 'Che1'); # 7 row(s) returned
+-- 5 Escreva uma query para exibir todas as informações de todos os projetos com mais de 500 horas.
+SELECT * FROM Scientists.Projects WHERE Hours > 500; # 5 row(s) returned
+-- 6 Escreva uma query para exibir todas as informações de todos os projetos cujas horas sejam maiores que 250 e menores 800.
+SELECT * FROM Scientists.Projects WHERE Hours BETWEEN 250 AND 800; # 7 row(s) returned
+SELECT * FROM Scientists.Projects WHERE Hours > 250 AND Hours < 800; # 7 row(s) returned
+-- 7 Escreva uma query para exibir o nome e o código de todos os projetos cujo nome NÃO inicie com a letra A.
+SELECT Name, Code FROM Scientists.Projects WHERE Name NOT LIKE 'A%'; # 4 row(s) returned
+-- 8 Escreva uma query para exibir o nome de todos os projetos cujo código contenha a letra H.
+SELECT Name FROM Scientists.Projects WHERE Code LIKE '%H%';
+
+-- > EXERCÍCIO do dia - 20.1 -- <---/ FIM -----------------------------------------//
+############################## Filtrando dados de forma específica
