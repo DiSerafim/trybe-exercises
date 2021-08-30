97579aee03914d7f2db2c14a33e36a67f11ad978
root <diegoserafim1@gmail.com>
2021-08-30 12:59:26 -0300

    26.2 concluido exercicio do dia

---
+++ back-end/bloco26/bloco26.2/exercicio/index3.js
@@ -0,0 +1,47 @@
+function doMath(a, b, c) {
+  return new Promise((resolve, reject) => {
+    // garante que todos os valores sejam numéricos
+    if (typeof a !== 'number' || typeof b !== 'number' || typeof c !== 'number')
+      //caso não seja 'number', rejeitamos
+      reject('Imforme apenas números');
+    // faz a soma
+    const resultado = (a + b) * c;
+    // valida o resultado se for maior que 50
+    if (resultado < 50) 
+      return reject('Valor muito baixo');
+    
+    resolve(resultado);
+  });
+}
+// Chama a função nas condições de entrada e verifica sua saída
+doMath(50, 10, 10)
+  .then(resolve => console.log(resolve))
+  .catch(error => console.log(error))
+// 200
+doMath(1, 1, 50)
+  .then(resolve => console.log(resolve))
+  .catch(error => console.log(error))
+// Imforme apenas números
+doMath(1, 50, 1)
+  .then(resolve => console.log(resolve))
+  .catch(error => console.log(error))
+  // Valor muito baixo
+
+// criar as funções para gerar números aleatórios..
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+async function callDoMath() {
+  /* Um novo array de 3 posições
+   * e utilizamos o `map` para gerar um número aleatório
+   * para cada posição do Array
+   */
+  const randomNumbers = Array.from({ length: 3 }).map(getRandomNumber);
+  // .. e chamar doMath
+  try {
+    const resultado = await doMath(...randomNumbers);
+    console.log(resultado);
+  } catch (err) {
+    console.log(err);
+  }
+}
