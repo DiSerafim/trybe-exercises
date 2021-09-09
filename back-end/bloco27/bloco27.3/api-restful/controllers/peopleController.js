bf650f9ed0bc46dcc1f77a7fd850aa364f7d53a6
Diego Serafim <diegoserafim1@gmail.com>
2021-09-09 14:44:25 -0300

    27.3 iniciado aula ao vivo

---
+++ back-end/bloco27/bloco27.3/api-restful/controllers/peopleController.js
@@ -0,0 +1,10 @@
+
+const getALL = async (req, res) => {
+  return res.send('funcionando!');
+}
+const getById = async (req, res) => {}
+const create = async (req, res) => {}
+const update = async (req, res) => {}
+const remove = async (req, res) => {}
+
+module.exports = { getALL, getById, create, update, remove };
