fdb33db83019d7b7953aea91229ac928cfca05bc
Diego Serafim <diegoserafim1@gmail.com>
2021-09-19 21:04:31 -0300

    28.2 Upload de arquivos com Multer, ao-vivo concluido

---
+++ back-end/bloco28/bloco28.2/client/index.js
@@ -0,0 +1,14 @@
+const FormData = require('form-data');
+const axios = require('axios');
+const fs = require('fs');
+
+const stream = fs.createReadStream('./meu-arquivo.txt');
+
+const formInfo = new FormData();
+formInfo.append('file', stream);
+
+const formHeader = formInfo.getHeaders();
+const URL = 'http://localhost:3000/files';
+
+axios.post(URL, formInfo, { headers: { ...formHeader } })
+  .then((response) => console.log(response));
