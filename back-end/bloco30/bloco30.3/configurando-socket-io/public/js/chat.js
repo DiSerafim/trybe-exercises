56e28e7e6e4fa90372ffbb3acd6e5c48d262b04d
root <diegoserafim1@gmail.com>
2021-10-22 20:29:26 -0300

    30.3 Sockets.io chat-real, conteudo

---
+++ back-end/bloco30/bloco30.3/configurando-socket-io/public/js/chat.js
@@ -0,0 +1,25 @@
+const socket = window.io();
+
+const form = document.querySelector('form');
+const inputMessage = document.querySelector('#messageInput');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  socket.emit('clientMessage', inputMessage.value);
+  inputMessage.value = '';
+  return false;
+});
+// O código a cima javascript determina que ao clicar no botão submit do formulário, será enviado um evento clientMessage com a mensagem preenchida no campo com id messageInput
+
+const createMessage = (message) => {
+  const messagesUl = document.querySelector('#messages');
+  const li = document.createElement('li');
+  li.innerText = message;
+  messagesUl.appendChild(li);
+};
+
+socket.on('serverMessage', (message) => createMessage(message));
+
+window.onbeforeunload = function(event) {
+  socket.disconnect();
+};
