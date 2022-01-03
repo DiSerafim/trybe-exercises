cd70d49020d55be8d66827b18f21ef045fc86471
Diego Serafim <diegoserafim1@gmail.com>
2022-01-02 22:08:40 -0300

    iniciado bloco33 - introducao-a-programacao-orientada-a-objetos

---
+++ ciencia-computacao/bloco_33/33.1-introducao-a-programacao-orientada-a-objetos/introducao-a-programacao-orientada-a-objetos.py
@@ -0,0 +1,100 @@
+# ########################################## introducao-a-programacao-orientada-a-objetos
+# --> CONTEÚDO do dia - 33.1 - <--- / INICIO ----------------------------- //
+# ########################################## introducao-a-programacao-orientada-a-objetos
+
+# Exemplo: recuperação de senha
+# Organizando a sua lógica em entidades
+# User, nossa primeira entidade
+# Enviar emails - onde coloco essa lógica?
+# Dicionário de conceitos
+# Mailer - Criando mais entidades
+# Dicionário de conceitos, parte dois
+# O que vem por aí
+
+""" ---> OBJETIVO <---
+- Definir Programação Orientada a Objetos como um paradigma de programação que é base para inúmeras arquiteturas de software
+- Descrever Programação Orientada a Objetos como um paradigma baseado na criação de entidades que interagem entre si através da troca de mensagens
+- Definir, em Programação Orientada a Objetos , uma Classe
+- Definir, em Programação Orientada a Objetos , um Construtor
+- Definir, em Programação Orientada a Objetos , um Objeto
+- Definir, em Programação Orientada a Objetos , uma Instância
+- Definir, em Programação Orientada a Objetos , um Atributo
+- Definir, em Programação Orientada a Objetos , um Método
+- Implementar, em Python , Classes , Instâncias , Atributos , Métodos e envio de Mensagens entre Objetos
+- Descrever, como pilar da Programação Orientada a Objetos , a Abstração - a criação de entidades que conterão, cada uma, parte da lógica de seu programa
+- Descrever, como pilar da Programação Orientada a Objetos , o Encapsulamento - a exposição somente do que é necessário para uso dos objetos de uma classe """
+
+
+# ---> Conteúdos <---
+
+"""  --------------------------------------------------------------------------- 
+ -> Exemplo: recuperação de senha
+---------------------------------------------------------------------------  """
+
+
+
+"""  --------------------------------------------------------------------------- 
+ -> Organizando a sua lógica em entidades
+---------------------------------------------------------------------------  """
+
+
+
+"""  --------------------------------------------------------------------------- 
+ -> User, nossa primeira entidade
+---------------------------------------------------------------------------  """
+
+
+
+"""  --------------------------------------------------------------------------- 
+ -> Enviar emails - onde coloco essa lógica?
+---------------------------------------------------------------------------  """
+
+
+
+"""  --------------------------------------------------------------------------- 
+ -> Dicionário de conceitos
+---------------------------------------------------------------------------  """
+
+
+
+"""  --------------------------------------------------------------------------- 
+ -> Mailer - Criando mais entidades
+---------------------------------------------------------------------------  """
+
+
+
+"""  --------------------------------------------------------------------------- 
+ -> Dicionário de conceitos, parte dois
+---------------------------------------------------------------------------  """
+
+
+
+"""  --------------------------------------------------------------------------- 
+ -> O que vem por aí
+---------------------------------------------------------------------------  """
+
+
+
+# --------------------------------------------------------------------------- #
+# - > CONTEÚDO do dia - 33.1 - <--- / FIM ---------------------------------- //
+# #####################################
+# - > AULA ao VIVO - 33.1 ----- <--- / INICIO ------------------------------ //
+# --------------------------------------------------------------------------- #
+
+# Foco de hoje
+# ...
+
+# --------------------------------------------------------------------------- #
+# - > AULA ao VIVO - 33.1 ----- <--- / FIM --------------------------------- //
+# #####################################
+# - > EXERCÍCIO do dia - 33.1 - <--- / INICIO ------------------------------ //
+# --------------------------------------------------------------------------- #
+
+# Agora a prática
+
+# Exercício 1: 
+
+# --------------------------------------------------------------------------- #
+# - > EXERCÍCIO do dia - 33.1 - <--- / FIM --------------------------------- //
+# ########################################## introducao-a-programacao-orientada-a-objetos
+# - Concluído ... ------------------------------------------------------------ #
