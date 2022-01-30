12dfbabbfff9dd88710cd48b04985ac610335b3f
Diego Serafim <diegoserafim1@gmail.com>
2022-01-29 21:01:59 -0300

    exercicio 36.4 concluido

---
+++ ciencia-computacao/bloco_36-estrutura-de-dados-arrays-hashmaps-sets/36.4-set/ex3.py
@@ -0,0 +1,9 @@
+# Continue os exercícios de fixação para terminar de implementar as operações de conjuntos:
+# difference;
+# issubset;
+# issuperset.
+# Desafio
+# Otimizar o algoritmo do exercício 2 para ter uma complexidade de tempo limite de O(n).
+
+
+""" ??? """
